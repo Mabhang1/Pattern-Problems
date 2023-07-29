fff5ba180a942ca0ff219eae259486d515cf3f78
Abhang Mane <maneabhang1@gmail.com>
2023-07-30 00:11:54 +0530

    smallchanges

--- squarehollow.java
+++ squarehollow.java
@@ -1,8 +1,8 @@
 public class squarehollow{
     public static void printPattern(int n){
         int i,j;
-        for(i=0; i<n;i++){
-            for(j=0; j<n ;j++){
+        for(i=0; i<n; i++){
+            for(j=0; j<n; j++){
                 if(i==0 || j==0 || i==n-1|| j==n-1){
                     System.out.print("*");
                 }
